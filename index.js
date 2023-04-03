024a6f284dd568bafeb274f66575b9d83b4df038
Bharatesh <bharateshpoojari@gmail.com>
2023-04-03 16:41:43 +0530

    Bharatesh's First Commit

---
+++ index.js
@@ -0,0 +1,18 @@
+var s1 = document.querySelector(".s1")
+var s2 = document.querySelector(".s2")
+var translate = document.querySelector(".translate")
+var url = "https://api.funtranslations.com/translate/yoda.json?text="
+
+translate.addEventListener("click",function fun(){
+    var input =s1.value
+    var finalurl = url+input
+    fetch(finalurl)//return  promise
+    .then(function print(pr){
+        return pr.json();
+    })
+    .then(function out(op){
+         s2.value = op.contents.translated
+
+    })
+})
+ 
